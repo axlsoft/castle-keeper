c1f588f001700d7c31fd65f64dbb115a1b506ca2
AJ <11035621+axlj45@users.noreply.github.com>
2025-02-08 07:40:32 -0500

    feat: fixed window state

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -10,13 +10,8 @@ fn greet(name: &str) -> Result<String, String> {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_window_state::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
-        .setup(|app| {
-            #[cfg(desktop)]
-            app.handle()
-                .plugin(tauri_plugin_window_state::Builder::default().build());
-            Ok(())
-        })
         .plugin(tauri_plugin_store::Builder::default().build())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
